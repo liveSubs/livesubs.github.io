a80817d093cf48fb8b23e00e39841e065fb24d18
Dima <65868322+byCodeXp@users.noreply.github.com>
2020-10-31 20:06:53 +0200

    upgrade api request

--- js/core.js
+++ js/core.js
@@ -1,6 +1,28 @@
 new Vue ({
-	el: '#sub_count',
+	el: '.user',
 	data: {
-		subs: "Hello World"
+		views: null,
+		subs: null,
+		videos: null,
+		name: null,
+		logo: null,
+		user_key: ''
+	},
+	methods: {
+		update: function () {
+			axios
+				.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=' + user_key + '&key=AIzaSyBzK5GgF-aScjm4MNJh_Fnuugyxowouz-0')
+				.then(response => (
+					this.views = response.data.items[0].statistics.viewCount,
+					this.subs = response.data.items[0].statistics.subscriberCount,
+					this.videos = response.data.items[0].statistics.videoCount,
+					this.name = response.data.items[0].snippet.title,
+					this.logo = response.data.items[0].snippet.thumbnails.medium.url
+				))
+		},
+		change: function (event) {
+			user_key = event.target.value;
+			this.update();
+		}
 	}
-});
+})
