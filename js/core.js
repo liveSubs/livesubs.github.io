98daa6c2b99e04070ab18b08ae84d59cc3ff499e
Dima <65868322+byCodeXp@users.noreply.github.com>
2020-11-03 13:59:02 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -11,19 +11,34 @@ new Vue ({
 	},
 	methods: {
 		update () {
-			const adrress = user_key;
-			const pattern = /\b(UC).{22}/;
-			const user = adrress.match(pattern);
-			axios
-				.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=' + user + '&key=AIzaSyBzK5GgF-aScjm4MNJh_Fnuugyxowouz-0')
-				.then(response => (
-					this.views = response.data.items[0].statistics.viewCount,
-					this.subs = response.data.items[0].statistics.subscriberCount,
-					this.videos = response.data.items[0].statistics.videoCount,
-					this.name = response.data.items[0].snippet.title,
-					this.logo = response.data.items[0].snippet.thumbnails.medium.url,
-					this.url = 'https://www.youtube.com/channel/' + response.data.items[0].id
+			const address = user_key;
+			if (address.length === 24) {
+				const pattern = /\b(UC).{22}/;
+				const user = address.match(pattern);
+				axios
+					.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=' + user + '&key=AIzaSyAvRN9xDLUISE5jR0wr92Vmz3Hwl_SvyxM')
+					.then(response => (
+						this.views = response.data.items[0].statistics.viewCount,
+						this.subs = response.data.items[0].statistics.subscriberCount,
+						this.videos = response.data.items[0].statistics.videoCount,
+						this.name = response.data.items[0].snippet.title,
+						this.logo = response.data.items[0].snippet.thumbnails.medium.url,
+						this.url = 'https://www.youtube.com/channel/' + response.data.items[0].id
 				))
+			}
+			else
+			{
+				axios
+					.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&forUsername=' + user_key + '&key=AIzaSyAvRN9xDLUISE5jR0wr92Vmz3Hwl_SvyxM')
+					.then(response => (
+						this.views = response.data.items[0].statistics.viewCount,
+						this.subs = response.data.items[0].statistics.subscriberCount,
+						this.videos = response.data.items[0].statistics.videoCount,
+						this.name = response.data.items[0].snippet.title,
+						this.logo = response.data.items[0].snippet.thumbnails.medium.url,
+						this.url = 'https://www.youtube.com/channel/' + response.data.items[0].id
+				))
+			}
 		},
 		change (event) {
 			user_key = event.target.value;
