138a154f736c4d4180ac8d80f8eea5e66377c8fd
Dima <65868322+byCodeXp@users.noreply.github.com>
2020-11-09 11:02:37 +0200

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,33 +1,32 @@
 new Vue ({
 	el: '.user',
 	data: {
-		views: null,
-		subs: null,
-		videos: null,
-		name: null,
-		logo: null,
-		url: null,
-		user_key: null
+		views: 0,
+		subs: 0,
+		videos: 0,
+		name: '',
+		logo: '',
+		url: '',
+		user_key: '',
+		result: 0
 	},
 	methods: {
 		update () {
 			const address = user_key;
-			if (address.length === 24) {
-				const pattern = /\b(UC).{22}/;
-				const user = address.match(pattern);
-				axios
-					.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=' + user + '&key=AIzaSyAvRN9xDLUISE5jR0wr92Vmz3Hwl_SvyxM')
-					.then(response => (
-						this.views = response.data.items[0].statistics.viewCount,
-						this.subs = response.data.items[0].statistics.subscriberCount,
-						this.videos = response.data.items[0].statistics.videoCount,
-						this.name = response.data.items[0].snippet.title,
-						this.logo = response.data.items[0].snippet.thumbnails.medium.url,
-						this.url = 'https://www.youtube.com/channel/' + response.data.items[0].id
-				))
-			}
-			else
-			{
+			const pattern = /\b(UC).{22}/;
+			const user = address.match(pattern);
+			axios
+				.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&id=' + user + '&key=AIzaSyAvRN9xDLUISE5jR0wr92Vmz3Hwl_SvyxM')
+				.then(response => (
+					this.views = response.data.items[0].statistics.viewCount,
+					this.subs = response.data.items[0].statistics.subscriberCount,
+					this.videos = response.data.items[0].statistics.videoCount,
+					this.name = response.data.items[0].snippet.title,
+					this.logo = response.data.items[0].snippet.thumbnails.medium.url,
+					this.url = 'https://www.youtube.com/channel/' + response.data.items[0].id,
+					this.result = response.data.pageInfo.resultsPerPage
+			))
+			if(true) {
 				axios
 					.get('https://www.googleapis.com/youtube/v3/channels?part=statistics&part=snippet&forUsername=' + user_key + '&key=AIzaSyAvRN9xDLUISE5jR0wr92Vmz3Hwl_SvyxM')
 					.then(response => (
